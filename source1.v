c379450143d1f5551ac5f99e40ffce1aa06fe5ad
Francois Ripault <francois.ripault@epita.fr>
2013-01-18 11:56:32 +0100

    Add few examples, start work on main section

---
+++ source1.v
@@ -0,0 +1,6 @@
+(** Ceci est une section de commentaire.
+    La ligne suivante définit une variable *)
+Definition maVariable : Set.
+(* Ceci est un commentaire n'apparaissant pas dans la
+   documentation, il ne commence que par une seule
+   étoile.*)
---
+++ source2.v
@@ -0,0 +1,36 @@
+(** * Ceci est un titre de niveau 1
+
+Les lignes qui suivent sont une liste
+- Un premier élément
+- Un second élément :
+    - Un sous liste d'un seul élément
+- Un dernier élément de cette liste
+
+La ligne suivante produit un séparateur horizontal :
+----
+
+Nous pouvons également mettre une emphase sur certains _mots_.
+
+Ainsi qu'insérer des règles de formattages spécifiques au
+fichier de sortie :
+% en \latex % $ en notation mathématique $ # en html #
+
+Nous pouvons également introduire des portions de code dans la
+documentation :
+<<
+  Definition maVariable : Set.
+>>
+
+La portion de code qui suit sera cachée dans le fichier de
+sortie *)
+(* begin hide *)
+
+Definition maVariable2 : Set.
+
+(* end hide *)
+
+(** Enfin, nous pouvons ajouter des règles pour transformer
+certains caractères dans le fichier de sortie grâce au mot clef
+printing : *)
+
+(** printing -> %\ensuremath{\rightarrow}% *)
