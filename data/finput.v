12796111f43c7973794686ad1359ccdf07f49030
Francois Ripault <francois.ripault@epita.fr>
2013-01-24 10:09:22 +0100

    Add more doc; Use cleveref

---
+++ data/finput.v
@@ -0,0 +1,7 @@
+(** * Fichier d'exemple Coq
+Cet exemple nous accompagne tout au long du traitement
+*)
+
+(* Nous definissons deux variables *)
+Parameter maVariable : Set.
+Parameter autreVariable : Set.
