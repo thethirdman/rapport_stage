b898e6e91633a5cc1f4207933a4d0fe69f57f9b6
Francois Ripault <francois.ripault@epita.fr>
2013-02-13 23:35:32 +0100

    Final push

---
+++ pres_pps/input.v
@@ -0,0 +1,17 @@
+(** * Example_doc
+  This is an example file
+
+----
+
+  Here is a list :
+  - With a first item
+  - And a second
+*)
+
+(* Now we write some code ! *)
+Definition id (n : nat) := n.
+
+(** This returns the _value 42_ *)
+Eval compute in id 42.
+
+
